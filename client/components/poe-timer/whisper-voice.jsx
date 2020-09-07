54612efcff810d94f4793ae6df745ba00845bbc6
nick-ng <nick.ng@nick.ng>
2020-09-08 08:27:42 +1200

    Added chooser for voice

--- client/components/poe-timer/whisper-voice.jsx
+++ client/components/poe-timer/whisper-voice.jsx
@@ -20,34 +20,55 @@ const Container = styled.div`
 
 const synth = window.speechSynthesis;
 
+const getDefaults = () => {
+  const defaultWhisperAge = parseInt(
+    localStorage.getItem(MAX_WHISPER_VOICE_AGE_SECONDS_KEY) ||
+      DEFAULT_MAX_WHISPER_VOICE_AGE_SECONDS,
+    10
+  );
+
+  const defaultSettings = JSON.parse(
+    localStorage.getItem(VOICE_SETTINGS_KEY) ||
+      JSON.stringify({
+        volume: 0.5,
+      })
+  );
+
+  return {
+    defaultWhisperAge,
+    defaultSettings,
+  };
+};
+
 export default function WhisperVoice({ event }) {
-  const [maxWhisperVoiceAgeSeconds, setMaxWhisperVoiceAgeSeconds] = useState(
-    parseInt(
-      localStorage.getItem(MAX_WHISPER_VOICE_AGE_SECONDS_KEY) ||
-        DEFAULT_MAX_WHISPER_VOICE_AGE_SECONDS,
-      10
-    )
-  );
-  const [voiceSettings, setVoiceSettings] = useState(
-    JSON.parse(
-      localStorage.getItem(VOICE_SETTINGS_KEY) ||
-        JSON.stringify({
-          volume: 0.5,
-        })
-    )
-  );
+  const { defaultWhisperAge, defaultSettings } = getDefaults();
+  const [maxWhisperAge, setMaxWhisperAge] = useState(defaultWhisperAge);
+  const [settings, setSettings] = useState(defaultSettings);
   const [voiceList, setVoiceList] = useState([]);
+  const [phrase, setPhrase] = useState(null);
   const [saying, setSaying] = useState("");
+
+  const updateSettings = (key, value) => {
+    setSettings({
+      ...settings,
+      [key]: value,
+    });
+  };
+
+  const getVoice = (voiceURI) =>
+    synth.getVoices().find((voice) => voice.voiceURI === voiceURI);
 
   useEffect(() => {
     function populateVoiceList() {
       const voices = synth.getVoices();
       if (voices && voices.length > 0) {
         setVoiceList(
-          voices.map((voice) => ({
-            voice,
-            display: `${voice.name} (${voice.lang})`,
-          }))
+          voices
+            .filter((a) => a.lang.startsWith("en"))
+            .map((voice) => ({
+              voice,
+              display: `${voice.name} (${voice.lang})`,
+            }))
         );
       }
     }
@@ -59,32 +80,66 @@ export default function WhisperVoice({ event }) {
   }, []);
 
   useEffect(() => {
-    const timestampThreshold = Date.now() - maxWhisperVoiceAgeSeconds * 1000;
+    const timestampThreshold = Date.now() - maxWhisperAge * 1000;
 
     if (
       event?.type === "whisper" &&
       event?.data &&
       event?.timestamp > timestampThreshold
     ) {
-      setSaying(event.data);
-      const utterance = new SpeechSynthesisUtterance(event.data);
-      utterance.volume = voiceSettings.volume;
-      synth.speak(utterance);
+      setPhrase(event.data);
     }
   }, [event]);
 
   useEffect(() => {
-    localStorage.setItem(VOICE_SETTINGS_KEY, JSON.stringify(voiceSettings));
-  }, [voiceSettings]);
+    if (phrase) {
+      setSaying(phrase);
+      const voice = getVoice(settings.voiceURI);
+      const utterance = new SpeechSynthesisUtterance(phrase);
+      utterance.volume = settings.volume;
+      if (voice) {
+        utterance.voice = voice;
+      }
+      synth.speak(utterance);
+    }
+  }, [phrase]);
+
+  useEffect(() => {
+    localStorage.setItem(VOICE_SETTINGS_KEY, JSON.stringify(settings));
+  }, [settings]);
 
   return (
     <Container>
-      <p>Voices:</p>
-      <ul>
-        {voiceList.map((voice) => (
-          <li key={voice.display}>{voice.display}</li>
-        ))}
-      </ul>
+      <div>
+        <label>
+          Trades only:
+          <input type="checkbox" />
+        </label>
+      </div>
+      <div>
+        <select
+          value={settings.voiceURI}
+          onChange={(e) => {
+            updateSettings("voiceURI", e.target.value);
+          }}
+        >
+          {voiceList.map((voice) => (
+            <option key={voice.voice.voiceURI} value={voice.voice.voiceURI}>
+              {voice.display}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={() => {
+            setPhrase(`Hello, this is ${settings.voiceURI}`);
+            setTimeout(() => {
+              setPhrase(null);
+            }, 100);
+          }}
+        >
+          Test
+        </button>
+      </div>
     </Container>
   );
 }
